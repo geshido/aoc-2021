6a0b16b4dd959ddb3fa8bcd5a25e610b3b2a69c4
Yury Ignatev <yury.ignatev@jetbrains.com>
2021-12-01 12:48:38 +0700

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,54 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var num =0
+        var prev = 0
+        input.map { it.toInt() }.forEach {
+            if (prev != 0 && it > prev) {
+                num++
+            }
+            prev = it
+        }
+        return num
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val ints = input.map { it.toInt() }
+
+        var num = 0
+        val windows = ints.windowed(3, 1, true)
+        windows.forEachIndexed { index, cur ->
+            if (index>0) {
+                if (cur.sum() > windows[index-1].sum()) {
+                    num++
+                }
+            }
+        }
+        return num
+    }
+
+    fun part2manual(input: List<String>): Int {
+        val ints = input.map { it.toInt() }
+        var i = 0
+        var j = 2
+        var num = 0
+        while (j < ints.size) {
+            if (i > 0) {
+                if (ints.subList(i-1, j).sum() < ints.subList(i, j+1).sum()) {
+                    num++
+                }
+            }
+            i++
+            j++
+        }
+        return num
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part2manual(testInput))
 
     val input = readInput("Day01")
-    println(part1(input))
+//    println(part1(input))
     println(part2(input))
+    println(part2manual(input))
 }
