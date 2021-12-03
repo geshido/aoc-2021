2949838d94860cd822f4e4e095f7dcf89624c837
Yury Ignatev <yury.ignatev@jetbrains.com>
2021-12-03 15:47:41 +0700

    day3 move find into class for part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -17,12 +17,19 @@ fun main() {
         val epsilon = Integer.parseUnsignedInt(buildString {
             counts.forEach { append(if (it > 0) "0" else "1") }
         }, 2)
-        println("gamma: $gamma")
-        println("epsilon: $epsilon")
 
         return gamma * epsilon
     }
 
+    data class Iter(val input: List<String>, val counts: List<Int>) {
+        fun find(bitIdx: Int, ok: Char.(count: Int) -> Boolean): String = when (input.size) {
+            1 -> input.first()
+            else -> {
+                val filtered = input.filter { it[bitIdx].ok(counts[bitIdx]) }
+                Iter(filtered, getCount(filtered)).find(bitIdx + 1, ok)
+            }
+        }
+    }
 
     fun part2(input: List<String>): Int {
         val counts = getCount(input)
@@ -30,32 +37,23 @@ fun main() {
         fun Char.oxygenOk(count: Int): Boolean = if (count >= 0) this == '1' else this == '0'
         fun Char.co2Ok(count: Int): Boolean = if (count >= 0) this == '0' else this == '1'
 
-        data class Iter(val input: List<String>, val counts: List<Int>)
-        fun find(initial: Iter, ok: Char.(count: Int) -> Boolean): List<String> =
-            initial.counts.foldIndexed(initial) { idx, iter, _ ->
-                if (iter.input.size > 1) {
-                    val newAcc = iter.input.filter { it[idx].ok(iter.counts[idx]) }
-                    Iter(newAcc, getCount(newAcc))
-                } else {
-                    iter
-                }
-            }.input
+        val oxygen = Iter(input, counts).find(0, Char::oxygenOk)
+        val co2 = Iter(input, counts).find(0, Char::co2Ok)
 
-        val oxygen = find(Iter(input, counts), Char::oxygenOk)
-        val co2 = find(Iter(input, counts), Char::co2Ok)
-        println(oxygen)
-        println(co2)
-
-        return Integer.parseUnsignedInt(oxygen.first(), 2) * Integer.parseUnsignedInt(co2.first(), 2)
+        return Integer.parseUnsignedInt(oxygen, 2) * Integer.parseUnsignedInt(co2, 2)
     }
 
     val testInput = readInput("Day03_test")
-//    check(part1(testInput) == 198)
-    val out = part2(testInput)
-    println(out)
-    check(out == 230)
+
+    val out1 = part1(testInput)
+    println("Test1: $out1")
+    check(out1 == 198)
+
+    val out2= part2(testInput)
+    println("Test2: $out2")
+    check(out2 == 230)
 
     val input = readInput("Day03")
-//    println(part1(input))
+    println("Part 1: ${part1(input)}")
     println("Part 2: ${part2(input)}")
 }
